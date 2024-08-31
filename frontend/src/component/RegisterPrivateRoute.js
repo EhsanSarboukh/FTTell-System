18b0c1fcbfabbb58429837bcafb8a6c188ec136b
Sabeel Hamood <116495058+Sabeel-Hamood@users.noreply.github.com>
2024-08-31 20:25:31 +0300

    Update RegisterPrivateRoute.js
    Adding documentation for RegisterPrivateRoute

--- frontend/src/component/RegisterPrivateRoute.js
+++ frontend/src/component/RegisterPrivateRoute.js
@@ -1,17 +1,17 @@
+// This component is intended to limit access to the relevant screens only to pediatricians
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 
 const RegisterPrivateRoute = ({ children }) => {
+  // Retrieve the 'canAccessRegister' flag from localStorage to check if the user has access permissions.
   const canAccessRegister = localStorage.getItem('canAccessRegister');
   const location = useLocation();
 
-  console.log('canAccessRegister:', canAccessRegister); 
-
+  // If the user tries to access the "/register" route and doesn't have the correct access flag, redirect them to "/login".
   if (location.pathname === '/register' && canAccessRegister !== 'true') {
-    console.log('Redirecting to login as canAccessRegister is not true');
     return <Navigate to="/login" />;
   }
-
+  // If the user has the correct permissions, render the children components.
   return children;
 };
 
