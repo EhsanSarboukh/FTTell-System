5dc2290e82be51a34d2110ac581f87a3b149e4ad
Ehsan Sarboukh <116456508+EhsanSarboukh@users.noreply.github.com>
2024-09-01 11:44:23 +0300

    Update footer.js

--- frontend/src/pages/footer.js
+++ frontend/src/pages/footer.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import styles from '../styles/footer.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone, faEnvelope  } from '@fortawesome/free-solid-svg-icons';
+import styles from '../styles/footer.css'; // Import the CSS file for styling
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesomeIcon component for icons
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'; // Import specific icons from FontAwesome
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p> FTTell</p>
+        <p>FTTell</p>
         <ul className="footer-links">
-    
           <li><a href="/about">About Us</a></li>
-          
           <li><a href="/FAQ">FAQ</a></li>
           <li><a href="/">Home</a></li>
-          <li><FontAwesomeIcon icon={faPhone} /> Contact us: 0527042828   </li> 
-          <li><FontAwesomeIcon  icon={faEnvelope} /> FTTell.diagnose@gmail.com</li>
-
-         
+          <li><FontAwesomeIcon icon={faPhone} /> Contact us: 0527042828</li> 
+          <li><FontAwesomeIcon icon={faEnvelope} /> FTTell.diagnose@gmail.com</li>
         </ul>
       </div>
     </footer>
   );
 }
 
-export default Footer;
+export default Footer; // Export the Footer component
