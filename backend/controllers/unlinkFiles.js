81be54b54771e6a944e3afa49b0431625aac7c1e
Ehsan Sarboukh <116456508+EhsanSarboukh@users.noreply.github.com>
2024-09-01 23:18:44 +0300

    Update unlinkFiles.js

--- backend/controllers/unlinkFiles.js
+++ backend/controllers/unlinkFiles.js
@@ -1,17 +1,19 @@
-function unLinkFiles(filePath ){
-    var fs = require('fs');
+function unLinkFiles(filePath) {
+    var fs = require('fs'); // Import the 'fs' (File System) module for file operations
    
-if (fs.existsSync(filePath)) {
-    fs.unlink(filePath, (err) => {
-        if (err) {
-            console.log(err);
-        }
-        console.log('deleted');
-    })
+    // Check if the file exists at the specified path
+    if (fs.existsSync(filePath)) {
+        // If the file exists, attempt to delete it
+        fs.unlink(filePath, (err) => {
+            if (err) {
+                console.log(err); // Log any errors that occur during the file deletion
+            } else {
+                console.log('deleted'); // Log a success message if the file was deleted
+            }
+        });
+    }
 }
-}
-
 
 module.exports = {
-    unLinkFiles
-  };
+    unLinkFiles // Export the unLinkFiles function for use in other parts of the application
+};
