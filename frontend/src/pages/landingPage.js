973a663aaef9f772139c10fd103b77054ae3a0dd
Ehsan Sarboukh <116456508+EhsanSarboukh@users.noreply.github.com>
2024-09-01 11:47:54 +0300

    Update landingPage.js

--- frontend/src/pages/landingPage.js
+++ frontend/src/pages/landingPage.js
@@ -1,38 +1,39 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Header from "./header";
-import styles from "../styles/landingPage.css"
-
+import { useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
+import Header from "./header"; // Import the Header component
+import styles from "../styles/landingPage.css"; // Import the CSS file for styling
 
 const LandingPage = () => {
-  localStorage.setItem('canAccessRegister', 'false'); // Set flag in local storage
-  const navigate = useNavigate();
+  // Set a flag in local storage to restrict access to the registration page
+  localStorage.setItem('canAccessRegister', 'false'); 
+  const navigate = useNavigate(); // Hook to navigate programmatically
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
-      navigate('/home'); 
+      navigate('/home'); // Navigate to the home page if the user is logged in
     }
   }, [navigate]);
 
   const handleClick = () => {
-    navigate('/login');
+    navigate('/login'); // Navigate to the login page on button click
   };
 
   return (
     <div className='containerLanding'>
       <Header />
-      
-      <div className='detailed'> <p>
-      Failure to thrive (FTT) is a frequently overlooked issue among infants<br></br> FTTell reduces the risk of misdiagnosing growth problems in children aged 0 to 60 months<br></br> This is achieved through the use of advanced technologies</p>
-      <button onClick={handleClick} >Diagnose Patient</button>
+      <div className='detailed'>
+        <p>
+          Failure to thrive (FTT) is a frequently overlooked issue among infants.
+          <br></br>
+          FTTell reduces the risk of misdiagnosing growth problems in children aged 0 to 60 months.
+          <br></br>
+          This is achieved through the use of advanced technologies.
+        </p>
+        <button onClick={handleClick}>Diagnose Patient</button>
       </div>
-
-
-     
-    
     </div>
   );
 };
 
-export default LandingPage;
+export default LandingPage; // Export the LandingPage component
