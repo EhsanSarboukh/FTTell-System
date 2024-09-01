c0472ab2686d85893ff06a745d28c5c4cebd16a9
Ehsan Sarboukh <116456508+EhsanSarboukh@users.noreply.github.com>
2024-09-01 11:47:20 +0300

    Update home.js

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,39 +1,38 @@
 // home.js
 import React from 'react';
-import Header from "./header";
-import { useNavigate} from 'react-router-dom';
-import   '../styles/home.css';
+import Header from "./header"; // Import the Header component
+import { useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
+import '../styles/home.css'; // Import the CSS file for styling
 
 const Home = () => {
-    localStorage.setItem('canAccessRegister', 'false'); // Set flag in local storage
+    // Set a flag in local storage to restrict access to the registration page
+    localStorage.setItem('canAccessRegister', 'false'); 
 
-    const navigate = useNavigate(); 
-
-
+    const navigate = useNavigate(); // Hook to navigate programmatically
 
     const handleClick = () => {
-        navigate('/diagnose');
-      };
+        navigate('/diagnose'); // Navigate to the diagnose page on button click
+    };
 
-    
-      return (
+    return (
         <div>
             <Header />
             <div className="home-container">
                 <h1 className="home-title">Welcome To FTTell</h1>
-                <p className="home-description">To diagnose a patient, start by entering the child's fetal weight and complete the required child information
-                <br></br>
-
-                For patients aged 6-25 months, an emotion recognition test requiring a connected camera is necessary<br></br> Ensure your camera is properly connected before starting the test<br></br>
-
-                <span className="highlight">Remember! , precise data entry is crucial for achieving accurate diagnostic results</span></p>
+                <p className="home-description">
+                    To diagnose a patient, start by entering the child's fetal weight and complete the required child information.
+                    <br></br>
+                    For patients aged 6-25 months, an emotion recognition test requiring a connected camera is necessary.
+                    <br></br> Ensure your camera is properly connected before starting the test.
+                    <br></br>
+                    <span className="highlight">Remember! Precise data entry is crucial for achieving accurate diagnostic results.</span>
+                </p>
                 <button className="home-button" onClick={handleClick}>
                     Diagnose Patient
                 </button>
             </div>
-          
         </div>
     );
 };
 
-export default Home;
+export default Home; // Export the Home component
