cffc26f682fb7551fbc95b140b082ad7dbdf7dfe
Ehsan Sarboukh <116456508+EhsanSarboukh@users.noreply.github.com>
2024-09-01 23:18:13 +0300

    Update jwtManager.js

--- backend/controllers/jwtManager.js
+++ backend/controllers/jwtManager.js
@@ -1,21 +1,17 @@
-require('dotenv').config();
-const jsonwebtoken = require("jsonwebtoken")
+require('dotenv').config(); // Load environment variables from a .env file
+const jsonwebtoken = require("jsonwebtoken"); // Import the jsonwebtoken module for handling JWTs
 
-const jwtManager = (user)=>
-{
-    const accessToken =  jsonwebtoken.sign(
+const jwtManager = (user) => {
+    // Generate a JWT (JSON Web Token) for the provided user
+    const accessToken = jsonwebtoken.sign(
         {
-            _id:user.id,
-            name:user.username,
-
+            _id: user.id,        // Include the user's ID in the payload
+            name: user.username, // Include the user's username in the payload
         },
-        process.env.jwt_salt
+        process.env.jwt_salt   // Use the secret key (salt) from the environment variable to sign the token
     );
 
-    return accessToken;
+    return accessToken; // Return the generated access token
+};
 
-}
-
-
-
-module.exports = jwtManager;
+module.exports = jwtManager; // Export the jwtManager function for use in other parts of the application
